935e59fabb0c1e072f091033e676286736bf77b2
raja <kunchala@yahoo.com>
2021-11-03 22:23:08 +0000

    Added Docket bean to limit the documentation

--- src/main/java/com/techreturners/bookmanager/BookmanagerApplication.java
+++ src/main/java/com/techreturners/bookmanager/BookmanagerApplication.java
@@ -2,12 +2,30 @@ package com.techreturners.bookmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class BookmanagerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BookmanagerApplication.class, args);
 	}
+
+	@Bean
+	public Docket swaggerConfiguration() {
+		return new Docket(DocumentationType.OAS_30)
+				.select()
+				.paths(PathSelectors.any())
+				.apis(RequestHandlerSelectors.
+						withClassAnnotation(RestController.class))
+				.build();
+	}
+
 
 }
